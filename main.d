6d137d9dd3850658f6ef74a8f53cd9abc9bced4d
MooMooCow1562 <106333842+MooMooCow1562@users.noreply.github.com>
2026-05-30 15:25:31 -0500

    main now reads any file in the ruleset folder
    added altRulesets for alternative rulesets
    renamed ruleset.json to ConwaysLife.json
    ghostyLife.json added, is a life variant with ghosts.

--- main.d
+++ main.d
@@ -5,27 +5,36 @@ import arsd.simpledisplay;
 import std.stdio : writeln;
 import life.squareLife;
 import arsd.jsvar;
-import std.file : readText;
+import std.array;
+import std.file;
 
 void main(){
-
-	var ruleset = var.fromJson(readText("ruleset/ruleset.json"));
+	//get our ruleset.
+	auto files = dirEntries("ruleset", "*.json", SpanMode.shallow).array;
+	var ruleset = var.fromJson(readText(files[0]));
+	//name our automata window (and make it)
 	auto window = new NVGWindow(800, 600, ruleset["automata name"]);
-	writeln(ruleset);
-	writeln(ruleset["board dimensions"]);
+	//get our dimensions.
 	auto dims = ruleset["board dimensions"].get!(int[]);
 	auto fullWidth = dims[0];
 	auto fullHeight = dims[1];
-	import std.random;
-	auto rnd = Random(7);
+	//initialize our life automata.
 	squareLife sq = new squareLife(dims[0], dims[1], 0, ruleset);
-	
+	//create a glider
 	sq.setCell(1, 1, 1);
 	sq.setCell(2, 1, 1);
 	sq.setCell(2, 2, 1);
 	sq.setCell(0, 2, 1);
 	sq.setCell(2, 3, 1);
-	
+	//square
+	sq.setCell(5, 5, 1);
+	sq.setCell(5, 6, 1);
+	sq.setCell(6, 5, 1);
+	sq.setCell(6, 6, 1);	
+	//oscilator
+	sq.setCell(25, 25, 1);
+	sq.setCell(25, 26, 1);
+	sq.setCell(25, 27, 1);
 	
 	window.redrawNVGScene = delegate (nvg){
 		for(int width = 0; width < fullWidth; width++){
@@ -39,7 +48,7 @@ void main(){
 		}
 	};
 	
-	window.eventLoop(1, delegate(){
+	window.eventLoop(50, delegate(){
 		sq.step();
 		window.redrawNVGSceneNow();
 	});
