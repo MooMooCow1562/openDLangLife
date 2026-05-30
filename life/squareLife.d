a21ac8dbd909a68f21d7833a6a4b59499c72ca2e
MooMooCow1562 <106333842+MooMooCow1562@users.noreply.github.com>
2026-05-30 14:30:01 -0500

    we have life.
    we have LIFE
    (had to track what types of neighbors I was looking for, adjust the json a bit, wrote squareLife.step and it's helpers.
    wrote main

--- life/squareLife.d
+++ life/squareLife.d
@@ -1,12 +1,14 @@
 module life.squareLife;
 import std.stdio;
+import arsd.jsvar;
+import std.algorithm.searching;
 
 class squareLife{
 	protected {
 		ubyte[] t_grid;
 		ubyte[] t_nextGrid;
 		int t_width;
-		int t_height;
+		int t_height;		
 	}
 	/**
 	Constructor, creates a grid.
@@ -23,6 +25,24 @@ class squareLife{
 		t_nextGrid = new ubyte[](width * height);
 		t_nextGrid[0 .. $] = initialValue;
 	}
+	/**
+	Constructor, creates a grid.
+	Params:
+		width = width of the grid.
+		height = height of the grid.
+		initialValue = the value to set all life cells to.
+		ruleset = the rules to store.
+	*/
+	this(int width = 5,	int height = 5, ubyte initialValue = 0, var ruleset){
+		parseRules(ruleset);
+		t_width = width;
+		t_height = height;
+		t_grid = new ubyte[](width * height);
+		t_grid[0 .. $] = initialValue;
+		t_nextGrid = new ubyte[](width * height);
+		t_nextGrid[0 .. $] = initialValue;
+	}
+	
 	protected{
 		/**
 		wraps a coordinate in a direction.
@@ -56,8 +76,8 @@ class squareLife{
 		y = the y location of the cell
 	*/
 	ubyte getCell(int x, int y){
-		write(x); write(" "); writeln(y);
-		writeln(y * t_width + x);
+		//write(x); write(" "); writeln(y);
+		//writeln(y * t_width + x);
 		return t_grid[y * t_width + x];
 	}
 	/**
@@ -94,15 +114,102 @@ class squareLife{
 	/**
 	copies the nextgrid onto the current grid and clears the nextgrid for the next step.
 	*/
+	
 	void shuffleOntoCurrent(){
 		t_grid[0 .. $] = t_nextGrid[0 .. $];
 		t_nextGrid = new ubyte[t_width * t_height];
 	}
 	/**
+	prints the grid, that's all.
 	*/
 	void printGrid(){
 	for(int i = 0; i < t_width; i++){
 			writeln(t_grid[i*t_height .. (i+1)*t_height]);
 		}
 	}
+	string[] t_cellNames;
+	string[][] t_aToB;
+	int[][] t_neighborsNeeded;
+	string[][] t_neighborsTypes;
+	bool[] t_kernelGrid;
+	int[] t_kernelCenter;
+	int[] t_kernelDimensions;
+	/**
+	sub in known values for search kernel.
+	*/
+	int countNeighbors(int x, int y, ubyte value){
+		return countNeighbors(x, y, t_kernelCenter[0], t_kernelCenter[1], t_kernelGrid, t_kernelDimensions[0], value);
+	}
+
+	void parseRules(var ruleset){
+		t_cellNames = ruleset["cell names"].get!(string[]);
+		writeln(t_cellNames);
+		
+		t_aToB = ruleset["rules"]["transformations"]["a->b"].get!(string[][]);
+		writeln(t_aToB);
+		
+		t_neighborsNeeded = ruleset["rules"]["transformations"]["nieghbors needed"].get!(int[][]);
+		writeln(t_neighborsNeeded);
+		
+		t_neighborsTypes = ruleset["rules"]["transformations"]["neigbor types tracked"].get!(string[][]);
+		
+		foreach(byte kernCell; ruleset["rules"]["kernel"]["grid"].get!(byte[])){
+			t_kernelGrid ~= cast(bool)kernCell;
+		}
+		writeln(t_kernelGrid);
+		
+		t_kernelCenter = ruleset["rules"]["kernel"]["center"].get!(int[]);
+		writeln(t_kernelCenter);
+		
+		t_kernelDimensions = ruleset["rules"]["kernel"]["dimensions"].get!(int[]);
+		writeln(t_kernelDimensions);
+	}
+
+	void step(){
+		//for every cell in order from the front (direction shouldn't matter.)
+		//(... shouldn't.)
+		foreach(int location, ubyte cell ; t_grid){
+			// There will always be a default of 0, so even if the rules don't handle something, a valid state will always be chosen.
+			ubyte nextCell = 0;
+			//for every cell type in order from the front
+			nameloop:
+			foreach(ubyte cellNameLoc, string cellName; t_cellNames){
+				//for every transformation rule in order from the front.
+				foreach(int tfLoc, string[] tf;t_aToB){
+					//if the cellname and cell location don't match
+					//or 
+					//the cell name and the transformation start don't match
+					if(cellNameLoc != cell || cellName != tf[0]){
+						//skip to next iteration.
+						continue;
+					}
+					//otherwise, if it's a 'for all cases' rule
+					//(-1 is a for all cases rule.)
+					if(t_neighborsNeeded[tfLoc][0] == -1){
+						//transform.
+						nextCell = cast(ubyte)countUntil(t_cellNames, tf[1]);
+						continue;
+					}
+					//otherwise
+					//count the neigbors
+					int neighbors = 0;
+					//count all neighbor types we need to track.
+					foreach(string neigborType ; t_neighborsTypes[tfLoc]){
+						neighbors += countNeighbors( location % t_width, location / t_width, cast(ubyte)countUntil(t_cellNames, neigborType));
+					}
+					//if we have the right amount of neigbors
+					if(canFind(t_neighborsNeeded[tfLoc],neighbors)){
+						//transform.
+						nextCell = cast(ubyte)countUntil(t_cellNames, tf[1]);
+						continue nameloop;
+					}
+				}
+			}
+			//set the cell location.
+			t_nextGrid[location] = nextCell;
+		}
+		//update the current grid.
+		shuffleOntoCurrent();
+	}
+
 }
--- main.d
+++ main.d
@@ -18,51 +18,35 @@ void main(){
 	auto fullHeight = dims[1];
 	import std.random;
 	auto rnd = Random(7);
-	squareLife sq = new squareLife(dims[0], dims[1], 0);
+	squareLife sq = new squareLife(dims[0], dims[1], 0, ruleset);
+	
+	sq.setCell(1, 1, 1);
+	sq.setCell(2, 1, 1);
+	sq.setCell(2, 2, 1);
+	sq.setCell(0, 2, 1);
+	sq.setCell(2, 3, 1);
+	
+	
 	window.redrawNVGScene = delegate (nvg){
-	for(int i = 0; i< fullWidth * fullHeight; i++){
-		sq.setCell(i / fullWidth, i %fullHeight, cast(ubyte)uniform(0, 2, rnd));
-	}
 		for(int width = 0; width < fullWidth; width++){
 			for(int height = 0; height < fullHeight; height++){
 				auto colors  = ruleset["cell colors"].get!(string[]);
 				nvg.beginPath();
-				nvg.strokeColor = NVGColor.green;
-				nvg.strokeWidth = 2;
 				nvg.fillColor = nvgRGB(colors[sq.getCell(width, height)]);
 				nvg.rect(width*10, height*10, 10, 10);
 				nvg.fill();
-				nvg.stroke();
 			}
 		}
 	};
 	
+	window.eventLoop(1, delegate(){
+		sq.step();
+		window.redrawNVGSceneNow();
+	});
+
 	window.eventLoop(0,
 		delegate (KeyEvent event) {
 			if (event == "*-Q" || event == "Escape") { window.close(); return; } // quit on Q, Ctrl+Q, and so on
 		},
 	);
-	testSQ();
-}
-void testSQ(){
-	squareLife sq = new squareLife();
-	sq.setCell(0, 0, 1);
-	sq.setCell(1, 0, 2);
-	sq.setCell(1, 1, 1);
-	sq.setCell(4, 4, 1);
-	sq.printGrid();
-	//wrapping test, offset places the top row and first column off the board.
-	assert(sq.countNeighbors(1, 1, 1, 1, [true, true, true, //1, 1, 1
-										  true, false, true,//1, 0, 1
-										  true, true, true],//1, 1, 1
-										  3, 1) == 1); //see if it only spots 1 of the two 1s.
-	assert(sq.countNeighbors(0, 0, 1, 1, [true, true, true, //1, 1, 1
-										  true, true, true, //1, 1, 1
-										  true, true, true],//1, 1, 1
-										  3, 1) == 3);
-	assert(sq.countNeighbors(0, 0, 1, 1, [true, true, true, //1, 1, 1
-										  true, false, true,//1, 0, 1
-										  true, true, true],//1, 1, 1
-										  3, 2) == 1);
-	assert(sq.getCell(1, 0) == 2); //see if we're writing to the right places.
 }
